712fa57be873bca0b1810bd9b76b4649101ac039
bacgrant <55911226+bacgrant@users.noreply.github.com>
2023-05-26 09:34:56 +0100

    update 9:35

--- tests/tests_Fars_functions.R
+++ tests/tests_Fars_functions.R
@@ -1,6 +1,11 @@
 
 library(testthat)
-source("~/Fars1/R/fars_functions.R")
+# source("~/Fars1/R/fars_functions.R")
+
+make_filename <- function(year) {
+    year <- as.integer(year)
+    sprintf("accident_%d.csv.bz2", year)
+}
 
 expect_that(make_filename(2013), is_a("character"))
 
